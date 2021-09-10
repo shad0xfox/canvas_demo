8c6f7502b04b876feae13d27ac8b73a58eb722d7
shad0xfox <l45632100@hotmail.com>
2021-09-11 02:29:37 +0800

    Add: vue init

---
+++ app/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+      "node": true
+    },
+    "extends": [
+      "plugin:vue/essential",
+      "eslint:recommended",
+      "@vue/prettier"
+    ],
+    "parserOptions": {
+      "parser": "babel-eslint"
+    },
+    "rules": {}
+  }
+  
